a47d55100840276eda16c30a0717319b9e424728
angelexi76 <angelasanchezbarbosa@gmail.com>
2021-02-13 23:13:50 -0500

    firts commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const search = document.getElementById('input-search')
+const box = document.getElementById('box-search')
+
+
+function buscador(){
+    filter =inputSearch.value.toupperCase();
+    li =box_search.getElementBytagName();
+}
+
+for (i = 0; i < li.length; i++){
+
+}
+function buscador()
